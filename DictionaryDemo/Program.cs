1362f2763ce21a43860cd176ba1281cc17261393
Gonca Gül Kazancı <gnc.kazanci@gmail.com>
2021-01-23 21:07:03 +0300

    Add project files.

---
+++ DictionaryDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DictionaryDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> books = new MyDictionary<int, string>();
+
+            books.Add(1, "Beyaz Zambaklar Ülkesinde");
+            books.Add(2, "Sefiller");
+            books.Add(3, "Dönüşüm");
+
+            books.GetList();
+
+            Console.Read();
+        }
+    }
+}
